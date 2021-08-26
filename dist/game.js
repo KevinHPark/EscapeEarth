8d20705ceea18b682f9897870fe3cd88f8c8a69f
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-08-25 21:51:00 -0500

    adds player movement

--- dist/game.js
+++ dist/game.js
@@ -5,7 +5,14 @@ var config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: [mainMenu, playerHouse]
+    scene: [mainMenu, playerHouse],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 0},
+            debug: false
+        }
+    }
 };
 // Create a new Phaser Game object
 var game = new Phaser.Game(config);
--- dist/scenes/player-house.js
+++ dist/scenes/player-house.js
@@ -4,25 +4,99 @@ const dialogLines = [
     "Welcome to Escape Earth; a game about escaping the terror of a non-sustainable world",
     "You play as a civilian named Tel"
 ]
+let dialogDisplayed = true;
 const playerHouse = new Phaser.Class({
     Extends: Phaser.Scene,
     
     initialize: function MainMenuScene (){
-        Phaser.Scene.call(this, {key: 'playerHouse'})
+        Phaser.Scene.call(this, {key: 'playerHouse'});
     },
-
+    dialogFinishedCallback: function (){
+        debugger
+        dialogDisplayed = false;
+    },
     preload: function() {
         this.load.plugin("DialogModalPlugin","./dialog_plugin.js");
         this.load.image('player-house','assets/player-house.png');
+
+        //TODO: load spritesheet for RPG, we can't use this one in a private-source game
+        this.load.spritesheet('dude',
+            'assets/dude.png',
+            {frameWidth: 32, frameHeight: 48}
+        );
     }, 
     create: function () {
         this.sys.install("DialogModalPlugin");
     
         this.add.image(400,300,'player-house');
     
-        //TODO: return a promise when I'm done displaying dialog
-        this.sys.dialogModal.init({dialogLines: dialogLines});
+        this.player = this.physics.add.sprite(100,450,'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.addPlayerAnimations();
 
+        //this.addWalls();
+        //this.physics.add.collider(this.player, this.walls)
+
+        
+        this.sys.dialogModal.init({dialogLines: dialogLines, dialogFinishedCallback: this.dialogFinishedCallback})
+            
+    },
+    update: function () {
+        let cursors = this.input.keyboard.createCursorKeys();
+        
+        if (!dialogDisplayed){
+            if (cursors.left.isDown) 
+            {
+                this.player.setVelocityX(-160);
+                this.player.anims.play('left',true);
+            }
+            else if (cursors.right.isDown)
+            {
+                this.player.setVelocityX(160);
+                this.player.anims.play('right', true);
+            }
+            else 
+            {
+                this.player.setVelocityX(0);
+                this.player.anims.play('turn');
+            }
+            if (cursors.up.isDown)
+            {
+                this.player.setVelocityY(-160)
+            }
+            else if(cursors.down.isDown)
+            {
+                this.player.setVelocityY(160)
+            }
+            else {
+                this.player.setVelocityY(0)
+            }
+        }
+        
+    },
+    
+    addPlayerAnimations()
+    {
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ {key: 'dude', frame: 4}],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8}),
+            frameRate: 10,
+            repeat: -1
+        });
     }
 });
 
